742bfbd6c1acd944b2c6d5aabd86b11954278b5e
bgoonz <bryan.guner@gmail.com>
2021-09-11 17:10:18 +0000

    update

---
+++ 33-js-concepts/index.js
@@ -0,0 +1,7 @@
+/* 
+    33 JavaScript Concepts is a project created to help JavaScript developers master their skills. It is a compilation of fundamental JavaScript concepts that are important and fundamental. 
+
+    This project was inspired by an article written by Stephen Curtis. 
+
+    Any kind of contribution is welcome. Feel free to contribute.
+*/
