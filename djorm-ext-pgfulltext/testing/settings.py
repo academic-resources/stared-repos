2eaf618b94b591ecd90e9acc5a3e508ce2f14244
bgoonz <bryan.guner@gmail.com>
2021-09-11 17:25:13 +0000

    update

---
+++ djorm-ext-pgfulltext/testing/settings.py
@@ -0,0 +1,37 @@
+import os
+import sys
+
+
+sys.path.insert(0, '..')
+
+PROJECT_ROOT = os.path.dirname(__file__)
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'test',
+        'USER': os.environ.get('POSTGRES_USER', 'postgres'),
+        'PASSWORD': '',
+        'HOST': os.environ.get('POSTGRES_HOST', 'postgresql'),
+        'PORT': '',
+    }
+}
+
+TIME_ZONE = 'America/Chicago'
+LANGUAGE_CODE = 'en-us'
+ADMIN_MEDIA_PREFIX = '/static/admin/'
+STATICFILES_DIRS = ()
+
+SECRET_KEY = 'di!n($kqa3)nd%ikad#kcjpkd^uw*h%*kj=*pm7$vbo6ir7h=l'
+INSTALLED_APPS = (
+    'djorm_pgfulltext',
+    'djorm_pgfulltext.tests',
+)
+
+import django
+if django.VERSION >= (1, 6):
+    TEST_RUNNER = 'django.test.runner.DiscoverRunner'
+else:
+    TEST_RUNNER = 'django.test.simple.DjangoTestSuiteRunner'
