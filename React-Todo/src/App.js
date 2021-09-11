ee79da4fee3345d35a4cb6292b073f3fcbf8ac7d
bgoonz <bryan.guner@gmail.com>
2021-09-11 17:48:31 +0000

    update

---
+++ React-Todo/src/App.js
@@ -0,0 +1,86 @@
+import React from "react";
+import Form from "../src/components/TodoComponents/TodoForm";
+import List from "../src/components/TodoComponents/TodoList";
+import "./components/TodoComponents/Todo.css";
+
+// you will need a place to store your state in this component.
+// design `App` to be the parent component of your application.
+// this component is going to take care of state, and any change handlers you need to work with your state
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: "",
+      todolist: [
+        // {
+        //   task: '',
+        //   id: '',
+        //   completed: false
+        // },
+      ],
+    };
+  }
+
+  handleInput = (event) => {
+    const { value } = event.target;
+    this.setState({
+      input: value,
+    });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    if (this.state.input) {
+      const obj = {
+        task: this.state.input,
+        id: Date.now(),
+        completed: false,
+      };
+      this.setState({
+        todolist: [...this.state.todolist, obj],
+        input: "",
+      });
+    }
+  };
+
+  crossout = (elementID) => {
+    this.setState({
+      todolist: this.state.todolist.map((item) => {
+        if (item.id === elementID) {
+          return {
+            task: item.task,
+            id: item.id,
+            completed: !item.completed,
+          };
+        } else {
+          return item;
+        }
+      }),
+    });
+  };
+
+  clearCompleted = (event) => {
+    event.preventDefault();
+    this.setState({
+      todolist: this.state.todolist.filter((item) => item.completed === false),
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Todo List: MVP</h1>
+        <List todolist={this.state.todolist} crossout={this.crossout} />
+        <Form
+          input={this.state.input}
+          handleSubmit={this.handleSubmit}
+          handleInput={this.handleInput}
+          clearCompleted={this.clearCompleted}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ React-Todo/src/components/TodoComponents/TodoList.js
@@ -0,0 +1,28 @@
+// your components will all go in this `component` directory.
+// feel free to change this component.js into TodoList.js
+
+import React from "react";
+import ToDo from "./Todo";
+
+const List = (props) => {
+  return (
+    <ul>
+      {props.todolist.map((newToDo) => {
+        return (
+          <ToDo
+            key={newToDo.id}
+            value={newToDo.task}
+            crossout={props.crossout}
+            id={newToDo.id}
+            completed={newToDo.completed}
+          />
+        );
+      })}
+    </ul>
+  );
+};
+
+export default List;
+
+//Must pass id=newToDo.id again because key cannot be used for anything other than setting the unique key value
+//to access id as a variable in Todo.js, we have to set it to id as well
