26858bf5edc7468ba871229aec603dcfbbe0cd7a
bgoonz <bryan.guner@gmail.com>
2021-09-11 17:11:20 +0000

    update

---
+++ algorithms.js/src/index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var lib = {
+  DataStructure: require('./data_structure'),
+  Graph: require('./graph'),
+  Math: require('./math'),
+  Search: require('./search'),
+  Sorting: require('./sorting'),
+  String: require('./string')
+};
+
+module.exports = lib;
